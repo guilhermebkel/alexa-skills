cb6c3d85d586734420f8a8db6f45e2a74e04db81
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2022-11-02 10:58:10 -0300

    chore(*): add correct code formating

--- src/Skills/OnePieceMangaSpoiler/index.ts
+++ src/Skills/OnePieceMangaSpoiler/index.ts
@@ -5,44 +5,44 @@ import OpexModule from "@/Skills/OnePieceMangaSpoiler/Modules/OpexModule"
 
 class OnePieceMangaSpoilerHandler implements Handler {
 	async onLaunch ({ responseBuilder }: HandlerProps): Promise<HandlerResponse> {
-    const spoilerInfo = await OpexModule.lookup()
+		const spoilerInfo = await OpexModule.lookup()
 
-    if (spoilerInfo.status === "available") {
-      return responseBuilder.speak("Temos spoiler essa semana!").getResponse()
-    }
-    
-    if (spoilerInfo.status === "manga-launched") {
-      return responseBuilder.speak("O mangá dessa semana já foi lançado!").getResponse()
-    }
+		if (spoilerInfo.status === "available") {
+			return responseBuilder.speak("Temos spoiler essa semana!").getResponse()
+		}
+		
+		if (spoilerInfo.status === "manga-launched") {
+			return responseBuilder.speak("O mangá dessa semana já foi lançado!").getResponse()
+		}
 
-    if (spoilerInfo.status === "not-found") {
-      return responseBuilder.speak("Nenhum spoiler foi encontrado nessa semana :(").getResponse()
-    }
-  }
+		if (spoilerInfo.status === "not-found") {
+			return responseBuilder.speak("Nenhum spoiler foi encontrado nessa semana :(").getResponse()
+		}
+	}
 
 	async onHelp ({ responseBuilder }: HandlerProps): Promise<HandlerResponse> {
-    return responseBuilder.speak("Need help?").getResponse()
-  }
+		return responseBuilder.speak("Need help?").getResponse()
+	}
 
 	async onCancelAndStop ({ responseBuilder }: HandlerProps): Promise<HandlerResponse> {
-    return responseBuilder.speak("Cancel and Stop!").getResponse()
-  }
+		return responseBuilder.speak("Cancel and Stop!").getResponse()
+	}
 
 	async onFallback ({ responseBuilder }: HandlerProps): Promise<HandlerResponse> {
-    return responseBuilder.speak("Fallbacked!").getResponse()
-  }
+		return responseBuilder.speak("Fallbacked!").getResponse()
+	}
 
 	async onSessionEnded ({ responseBuilder }: HandlerProps): Promise<HandlerResponse> {
-    return responseBuilder.speak("Ended session!").getResponse()
-  }
+		return responseBuilder.speak("Ended session!").getResponse()
+	}
 
 	async onIntentReflector ({ responseBuilder }: HandlerProps): Promise<HandlerResponse> {
-    return responseBuilder.speak("Reflected intent!").getResponse()
-  }
+		return responseBuilder.speak("Reflected intent!").getResponse()
+	}
 
 	async onError ({ responseBuilder }: HandlerProps): Promise<HandlerResponse> {
-    return responseBuilder.speak("Failed!").getResponse()
-  }
+		return responseBuilder.speak("Failed!").getResponse()
+	}
 }
 
 export const main = HandlerModule.adapt(new OnePieceMangaSpoilerHandler())
