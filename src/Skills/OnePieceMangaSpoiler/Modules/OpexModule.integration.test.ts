59aee6832641f2827fd1ebb909263488976c4ce7
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2022-11-20 09:43:13 -0300

    chore(one-piece): improve name

--- src/Skills/OnePieceMangaSpoiler/Modules/OpexModule.integration.test.ts
+++ src/Skills/OnePieceMangaSpoiler/Modules/OpexModule.integration.test.ts
@@ -3,9 +3,9 @@ import OpexModule from "@/Skills/OnePieceMangaSpoiler/Modules/OpexModule"
 describe("OpexModule", () => {
 	describe("lookup()", () => {
 		test("Should not throw when looking up a spoiler", async () => {
-			const lookupPromise = OpexModule.getSpoilerInfo()
+			const getSpoilerInfoPromise = OpexModule.getSpoilerInfo()
 
-			await expect(lookupPromise).resolves.not.toThrow()
+			await expect(getSpoilerInfoPromise).resolves.not.toThrow()
 		})
 	})
 })
